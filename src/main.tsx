b4dd29bd08f305499126574fe734ff4892ad2d44
yiKyo <yikyo666@gmail.com>
2022-10-10 09:19:20 +0800

    feat: 添加 tailwind 相关配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
+import './index.css';
+
 import React from 'react';
-import App from './App';
 import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from './App';
+
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
