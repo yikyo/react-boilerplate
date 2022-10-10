2499d67d45cdc18d9534bc3d81f1aec4ab581c91
yiKyo <yikyo666@gmail.com>
2022-10-10 11:16:37 +0800

    feat: 更新lint-stage、tailwindcss 配置

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,7 +1,4 @@
 module.exports = {
   'src/**/*{md,json}': ['prettier --write'],
-  'src/**/*.ts?(x)': [
-    'prettier --parser=typescript --write',
-    'eslint --quiet --fix',
-  ],
+  'src/**/*.{ts,tsx}': ['prettier --parser=typescript --write', 'eslint --fix'],
 };
